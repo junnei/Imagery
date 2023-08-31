16f4b02306bcf605d311f1bc59916c55d42a482d
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-09-01 04:57:48 +0900

    [Chore] Create Xcode project

---
+++ Imagery/ImageryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageryApp.swift
+//  Imagery
+//
+//  Created by Jun on 2023/09/01.
+//
+
+import SwiftUI
+
+@main
+struct ImageryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
