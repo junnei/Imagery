c2b7faa29c675ab730862554aa1f9a16966f88a0
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-09-02 15:21:07 +0900

    [Add] Speech Text

--- Imagery/Model/GameManager.swift
+++ Imagery/Model/GameManager.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import AVFoundation
 
 class GameManager : ObservableObject {
     static let shared = GameManager()
+    let speechSynthesizer = AVSpeechSynthesizer()
     private init() {}
     
     @Published var gameState: gameState = .initial
