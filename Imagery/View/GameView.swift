39b3cecd68645e0ebebd698c2b2fb1c608227b52
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-14 22:42:56 +0900

    [Design] Update UX Writing for IllustView

--- Imagery/View/GameView.swift
+++ Imagery/View/GameView.swift
@@ -336,7 +336,11 @@ private extension GameView {
                 Spacer()
                     .frame(width: 390, height: 390)
                 
-                Text("일러스트에 손을 가져다대어 그림을 느껴보세요")
+                VStack {
+                    Text("일러스트에 손을 가져다대어 그림을 느껴보세요")
+                    Text("현재 누르고 있는 이미지의 색의 명도에 따라")
+                    Text("밝을수록 진동이 세게 울려요.")
+                }
                     .font(.headline)
                     .fontWeight(.semibold)
                     .foregroundColor(Color.OasisColors.darkGreen)
